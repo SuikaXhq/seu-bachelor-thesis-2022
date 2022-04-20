c0b8ebc21f203acd399b613197097547dca9b4a4
SuikaXhq <xuhaoqing2013@hotmail.com>
2022-04-20 17:15:17 +0800

    调整浮动体格式

---
+++ sample_thesis.bat
@@ -0,0 +1,14 @@
+del seuthesis-2019.cls
+del seuthesis-2019.cfg
+del seuthesisdoc.sty
+del *.log
+del *.aux
+del *.toc
+del *.bbl
+del *.out
+del *.blg
+del *.listing
+del *.synctex.gz
+xelatex seuthesis-2019.ins
+xelatex -interaction=nonstopmode -file-line-error -shell-escape sample_thesis
+xelatex -interaction=nonstopmode -file-line-error -shell-escape sample_thesis
