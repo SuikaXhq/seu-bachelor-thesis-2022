795973d41befca8f5042b205c7f553b2f8a88690
Xhq <xuhaoqing2013@hotmail.com>
2022-04-23 03:24:57 +0800

    finished document and bug fix

--- seuthesis-2022.bat
+++ seuthesis-2022.bat
@@ -1,6 +1,18 @@
 del seuthesis-2022.cls
 del seuthesis-2022.cfg
 del seuthesisdoc.sty
+del seuthesis-2022.aux
+del seuthesis-2022.glo
+del seuthesis-2022.gls
+del seuthesis-2022.idx
+del seuthesis-2022.ind
+del seuthesis-2022.ilg
+del seuthesis-2022.log
+del seuthesis-2022.out
+del seuthesis-2022.toc
 xelatex seuthesis-2022.ins
+xelatex seuthesis-2022.dtx
+makeindex -s gind.ist -o seuthesis-2022.ind seuthesis-2022.idx
+makeindex -s gglo.ist -o seuthesis-2022.gls seuthesis-2022.glo
 xelatex seuthesis-2022.dtx
 xelatex seuthesis-2022.dtx
