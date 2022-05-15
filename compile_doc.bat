9545d9f15e0c9cdab9b2ea379572381de71a1be6
SuikaXhq <xuhaoqing2013@hotmail.com>
2022-05-15 20:16:07 +0800

    Release 1.0.4

--- compile_doc.bat
+++
@@ -1,6 +0,0 @@
-xelatex seuthesis-2022.ins
-xelatex seuthesis-2022.dtx
-makeindex -s gind.ist -o seuthesis-2022.ind seuthesis-2022.idx
-makeindex -s gglo.ist -o seuthesis-2022.gls seuthesis-2022.glo
-xelatex seuthesis-2022.dtx
-xelatex seuthesis-2022.dtx
